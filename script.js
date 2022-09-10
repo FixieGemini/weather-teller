c4fe9911fe6b3256a17f5f0605a0aafd5e9f7113
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-09-10 12:25:22 -0700

    adding function and event listener to generat list of cities searched

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ var cityName = "tucson"
 
 let date = moment().format("MMM DD, YYYY");
 
+const addCities = $('.add-cities');
+const citiesList = $('.cites');
+const cities = JSON.parse(localStorage.getItem('cities')) || [];
+
+function addCity(event) {
+    event.preventDefault();
+    const text = (this.$('name=city')).value;
+    const item = {
+        text
+    };
+
+    cities.push(item);
+    populateList(city, citiesList);
+    localStorage.setItem('items', JSON.stringify(cities));
+    this.reset();
+}
+
+function populateList(cities = [], citiesList) {
+    citiesList.innerHTML = cities.map((city, i) => {
+        return `
+        <li>
+        <label for='city${i}'>${city.text}</label>
+        <li>
+        `;
+    });
+}
+
+addCities.addEventListener('btn', addCity);
+populateList(cities, citiesList);
+
 function getApi() {
     var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
   
@@ -26,7 +56,7 @@ function getApi() {
 
 
   function getWeather(lat, lon, currentCity) {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
   
     fetch(requestUrl)
       .then(function (response) {
@@ -35,10 +65,10 @@ function getApi() {
       .then(function (data) {
         console.log(data);
         
-        var temp = data.current.temp;
-        var humidity = data.current.humidity;
-        var windSpeed = data.current.wind_speed;
-        var uvi = data.current.uvi;
+        var temp = data.main.temp;
+        var humidity = data.main.humidity;
+        var windSpeed = data.wind.speed;
+        var uvi = data.main.uvi;
 
         document.getElementById('current').innerHTML =
 
@@ -49,22 +79,36 @@ function getApi() {
             <p>Wind Speed: <span class="current" id="wind-speed">${windSpeed}</span></p>
             <p>UV index: <span class="current rounded py-2 px-2 text-white" id="uv-index"></span>${uvi}</p>
         </div>` 
+        getForecast(lat, lon)
+    });
 
-        for(let i = 0; i < 5; i++) {
-            $("#fiveday").append(
-        
-            `<div class="col-sm-2 bg-primary text-light m-2 p-2">
-                <p>${date}</p>
-                <p></p>
-                <p>Temp:<span> ${data.daily[i].temp.day}</span></p>
-                <p>Wind:<span> ${data.daily[i].wind_speed}</span></p>
-                <p>Humidity:<span> ${data.daily[i].humidity}</span></p>
-            </div>`
-            )
-        }
+    function getForecast(lat, lon) {
+        var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
+    for(let i = 0; i < 5; i++) {
+        $("#fiveday").append(
+    
+        `<div class="col-sm-2 bg-primary text-light m-2 p-2">
+            <p>${i + 1}</p>
+            <p></p>
+            <p>Temp:<span> ${data.list[i].main.temp}</span></p>
+            <p>Wind:<span> ${data.list[i].wind.speed}</span></p>
+            <p>Humidity:<span></span></p>
+        </div>`
+        )
+    
+    }
 
     });
 
-  }
+    }
+}
 
 getApi();
